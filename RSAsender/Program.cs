6bc898f64a9f7aecc91704c99acc27e1489ab970
Jesper Klejs Deleurand (JESP446C) <jesperd55@gmail.com>
2021-01-07 12:13:09 +0100

    Initial commit

---
+++ RSAsender/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+namespace RSAsender
+{
+    class Program
+    {
+        const string publicKeyPath = "c:\\temp\\publickey.xml";
+        const string privateKeyPath = "c:\\temp\\privatekey.xml";
+        static void Main(string[] args)
+        {
+            var encrypter = new Encrypter();
+            while (true)
+            {
+                Console.WriteLine("Type string to encrypt: ");
+                var dataToEncrypt = Console.ReadLine();
+
+                var encrypted = encrypter.EncryptData(publicKeyPath, Encoding.UTF8.GetBytes(dataToEncrypt));
+                Console.WriteLine();
+                Console.WriteLine("Encrypted string:");
+                Console.WriteLine(Convert.ToBase64String(encrypted));
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
